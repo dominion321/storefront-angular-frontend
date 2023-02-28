8992d67afea4b3b591addfdb53b86f473eb00c0b
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-02-28 10:30:56 +0100

    added a redirect route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'cart', component: CartComponent },
   { path: 'detail', component: ProductItemDetailComponent },
   { path: 'confirmation', component: ConfirmationComponent },
+  { path: '**', redirectTo:'/' },
 ];
 
 @NgModule({
